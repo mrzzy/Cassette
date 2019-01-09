34724ad2a5d5600fd250f36cd3e90c38fb33ac26
ossaris56 <ossaris56@gmail.com>
2019-01-10 01:50:17 +0800

    actual final commit

--- app.py
+++ app.py
@@ -63,4 +63,4 @@ def audioprocess():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8234, ssl_context='adhoc')
+    app.run(debug=True, port=8234, ssl_context='adhoc', host="0.0.0.0")
