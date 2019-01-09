23eaf1e41b25a97823cf346365f21708d3ee2bd2
ossaris56 <ossaris56@gmail.com>
2019-01-09 12:36:01 +0800

    mobile 'friendly' + sidebar

---
+++ static/pleaseendmysuffering.js
@@ -0,0 +1,18 @@
+function closeNav() {
+  document.getElementById("legendSideBar").style.width = "0";
+  
+}
+
+function openNav() {
+  document.getElementById("legendSideBar").style.width = "200px";
+  $(document).mouseup(function(e) 
+    {
+        var container = $("legendSideBar");
+
+        // if the target of the click isn't the container nor a descendant of the container
+        if (!container.is(e.target) && container.has(e.target).length === 0) 
+        {
+            closeNav();
+        }
+    });
+}
