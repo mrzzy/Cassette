573e61afcf2a51a16443ec85e966779d00106a33
ossaris56 <ossaris56@gmail.com>
2019-01-10 10:58:12 +0800

    now its actually accurate

--- static/pleaseendmysuffering.js
+++ static/pleaseendmysuffering.js
@@ -24,3 +24,10 @@ function deleteDivs() {
 function showTrashcan() {
   document.getElementById("deleteBtn").style.display = "block";
 }
+
+window.addEventListener("load",function() {
+    setTimeout(function(){
+        // This hides the address bar:
+        window.scrollTo(0, 1);
+    }, 0);
+});
